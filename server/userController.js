c29dc34f4cf088bca9cabc5176cd0c3f33062562
Lizok-Tikhonova <97902948+Lizok-Tikhonova@users.noreply.github.com>
2024-05-05 22:36:57 +0300

    логика проверки авторизованности

---
+++ server/userController.js
@@ -0,0 +1,67 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const {User, Basket} = require('../models/models')
+
+const generateJwt = (id, login, role) => {
+    return jwt.sign(
+        {id, login, role},
+        'secret_key',
+        {expiresIn: '24h'} //сколько живет токен
+    )
+}
+
+
+class UserControler{ //для группировки
+    async registration(req,res){
+        try{
+            const {login, password, role} = req.body
+            if(!login || !password){
+                return res.status(400).json({message: 'Поля логина или пароля не могут быть пустыми'})
+            }
+            const userCheck = await User.findOne({where: {login}})
+            if(userCheck){
+                return res.status(400).json({message: 'Пользователь с таким именем уже существует'})
+            }
+
+            const hashPassword = await bcrypt.hash(password, 5) // хэшируем пароль, чтобы не хранить в открытиом доступе(5-сколько раз хэшируем)
+            const user = await User.create({login, role, password: hashPassword})
+            const basket = await Basket.create({userId: user.id, login, role})
+
+            console.log(user);
+            const token = generateJwt(user.id, user.login, user.role)
+            return res.json({token})
+
+        } catch(e){
+            console.log(e);
+            res.status(400).json({message: 'Ошибка регистрации'})
+        }
+        
+    }
+
+    async login(req,res){
+        try{
+            const {login, password} = req.body
+            const user = await User.findOne({where:{login}})
+            if(!user){
+                return res.status(400).json({message: 'Пользователь с таким именем не найден'})
+            }
+            let checkPassword = bcrypt.compareSync(password, user.password) // проверка, что пароль совпадает с захэшированным из базы
+            if(!checkPassword){
+                return res.status(400).json({message: 'Указан не верный пароль'})
+            }
+            const token = generateJwt(user.id, user.login, user.role)
+            return res.json({token})
+        } catch(e){
+            console.log(e);
+            res.status(400).json({message: 'Ошибка авторизации'})
+        }
+        
+    }
+
+    async check(req, res){
+        const token = generateJwt(req.user.id, req.user.login, req.user.role)
+        res.json({token})
+    }
+}
+
+module.exports = new UserControler()
